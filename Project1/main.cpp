6862c2da711f1e2e4d9b15412d5a03c3c470a6ba
Charles Wang <Charles@0587383115.wireless.umich.net>
2017-05-03 14:26:03 -0400

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Charles Wang on 5/3/17.
+//  Copyright © 2017 Charles Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
